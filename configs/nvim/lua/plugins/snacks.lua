7c66a83f6d14e27e416cd2a4ce02b2d4cf0e79dd
Frank Iannelli <friannelli@seek.com.au>
2025-07-22 14:54:15 +1000

    show hidden files in explorer and fuzzy finder

---
+++ configs/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts.picker = opts.picker or {}
+
+      opts.picker.hidden = true
+      opts.picker.ignored = true
+
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.files = opts.picker.sources.files or {}
+      opts.picker.sources.files.hidden = true
+
+      return opts
+    end,
+  },
+}
